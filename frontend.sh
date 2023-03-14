d79c39a955abd7c5188f1c7bf05f1f70ed63c4de
nikhilt <nikhil.t1226@gmail.com>
2023-03-14 21:37:35 +0000

    learning print.sh

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+code_dir=$(pwd)
 echo -e "\e[35mInstalling ngnix\e[0m"
 yum install nginx -y
 
@@ -12,9 +13,7 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[35mCopying nginx config for Roboshop\e[0m"
-pwd
-ls -l
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35mEnabling nginx\e[0m"
 systemctl enable nginx
