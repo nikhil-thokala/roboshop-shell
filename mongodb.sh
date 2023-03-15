7e983970cf1a3d396b3c9559b0aa9a526a40cadb
nikhilt <nikhil.t1226@gmail.com>
2023-03-15 03:31:44 +0000

    update

--- mongodb.sh
+++ mongodb.sh
@@ -1,18 +1,18 @@
 source common.sh
 
 print_head "setup Mongodb repository"
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Install Mongodb"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>${log_file}
 
 print_head "Update MongoDB Listen address"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log_file}
 
 print_head "enabling Mongodb"
-systemctl enable mongod
+systemctl enable mongod &>>${log_file}
 
 print_head "Start Mongodb Service"
-systemctl start mongod
+systemctl start mongod &>>${log_file}
 
 # update /etc/mongod.conf file from 127.0.0.1 with 0.0.0.0
