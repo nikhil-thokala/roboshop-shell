3c2b9726f9992fc01f4fe0d15c06ec2629bed2e0
nikhilt <nikhil.t1226@gmail.com>
2023-03-15 03:28:50 +0000

    update

--- mongodb.sh
+++ mongodb.sh
@@ -6,6 +6,9 @@ cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 print_head "Install Mongodb"
 yum install mongodb-org -y
 
+print_head "Update MongoDB Listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 print_head "enabling Mongodb"
 systemctl enable mongod
 
