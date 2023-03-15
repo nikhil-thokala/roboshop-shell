1339bf93ff88a3744b5ebb9af8ede0f33a58970c
nikhilt <nikhil.t1226@gmail.com>
2023-03-15 03:32:43 +0000

    update

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 print_head "setup Mongodb repository"
-cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "Install Mongodb"
 yum install mongodb-org -y &>>${log_file}
