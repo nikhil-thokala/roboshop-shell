7e189cae52a55f7be639eda11270730cca979165
nikhilt <nikhil.t1226@gmail.com>
2023-03-16 13:17:45 +0000

    update

--- redis.sh
+++ redis.sh
@@ -17,10 +17,10 @@ sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>${log_file}
 status_check $?
 
 print_head "Enable Redis"
-systemctl enable redis
+systemctl enable redis &>>${log_file}
 status_check $?
 
 print_head "Restart Redis"
-systemctl restart redis
+systemctl restart redis &>>${log_file}
 status_check $?
 
