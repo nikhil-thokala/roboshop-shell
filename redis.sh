55a0b2fc828900613719221bc3998a043905983c
nikhilt <nikhil.t1226@gmail.com>
2023-03-16 13:04:27 +0000

    update

---
+++ redis.sh
@@ -0,0 +1,26 @@
+source.common.sh
+
+print_head "Installing Redis Repo Files"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status_check $?
+
+print_head "Enabling 6.2 Redis Repo"
+dnf module enable redis:remi-6.2 -y &>>${log_file}
+status_check $?
+
+print_head "Installing Redis"
+yum install redis -y &>>${log_file}
+status_check $?
+
+print_head "Update Redis Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>${log_file}
+status_check $?
+
+print_head "Enable Redis"
+systemctl enable redis
+status_check $?
+
+print_head "Restart Redis"
+systemctl restart redis
+status_check $?
+
