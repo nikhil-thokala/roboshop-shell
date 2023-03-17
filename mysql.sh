c6d63485973a2a385be7328373d536ae015bd9ee
nikhilt <nikhil.t1226@gmail.com>
2023-03-17 02:34:44 +0000

    \ |"?

--- mysql.sh
+++ mysql.sh
@@ -23,5 +23,5 @@ systemctl start mysqld
 status_check$?
 
 print_head"Set Root Password"
-mysql_secure_installation --set-root-pass $(mysql_root_password)
+mysql_secure_installation --set-root-pass ${mysql_root_password}
 status_check$?
