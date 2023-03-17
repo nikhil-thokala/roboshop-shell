3fbb9edd2386e28f34ab58e15a65ca498c7fc333
nikhilt <nikhil.t1226@gmail.com>
2023-03-17 02:44:26 +0000

    \ |"?

--- mysql.sh
+++ mysql.sh
@@ -8,24 +8,24 @@ fi
 
 print_head "Disabling MySQL 8 Version"
 dnf module disable mysql -y &>>${log_file}
-status_check$?
+status_check $?
 
 print_head "Copying MySQl Repo File"
 cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
-status_check$?
+status_check $?
 
 print_head "Installing MySQL Server"
 yum install mysql-community-server -y &>>${log_file}
-status_check$?
+status_check $?
 
 print_head "Enabling MySQL Service"
 systemctl enable mysqld &>>${log_file}
-status_check$?
+status_check $?
 
 print_head "Start MySQL Service"
 systemctl start mysqld &>>${log_file}
-status_check$?
+status_check $?
 
 print_head "Set Root Password"
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
-status_check$?
+status_check $?
