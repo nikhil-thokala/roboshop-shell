c9aa725c796d875b05d8a7cb6e8e742e8cda08a9
nikhilt <nikhil.t1226@gmail.com>
2023-03-17 02:50:30 +0000

    \ |"?

--- mysql.sh
+++ mysql.sh
@@ -27,5 +27,8 @@ systemctl start mysqld &>>${log_file}
 status_check $?
 
 print_head "Set Root Password"
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+echo show databases | mysq1 -uroot -p${mysql_uroot_password} &>>${log.file}
+if [ $? -ne 0 ]; then
+   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+fi
 status_check $?
