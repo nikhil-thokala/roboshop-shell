08d23eb4120fe1b85f9004d3cfc746d36b762279
nikhilt <nikhil.t1226@gmail.com>
2023-03-19 08:41:56 +0000

    \ |"?

--- common.sh
+++ common.sh
@@ -58,7 +58,7 @@ schema_setup() {
     status_check $?
 
     print_head "Load Schema"
-    mysql -h mysql.devopsjob.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
+    mysql -h mysql-dev.devopsjob.online -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
     status_check $?
   fi
 }
